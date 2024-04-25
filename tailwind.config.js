5a3e4b0f516fc53582540cbb1a84fad93bd14476
Dawid Golen <dawigol712@student.polsl.pl>
2024-04-25 12:00:49 +0200

    add native tailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#333333",
+        secondary: "#B3B3B3",
+        tertiary: "#FFFFFF",
+        quaternary: "#5A91E0",
+      },
+    },
+  },
+  plugins: [],
+};
